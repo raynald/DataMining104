39a695497b71605da9fd020f16fce5a2832864a8
Raynald Chung <raynaldchung@gmail.com>
2014-05-21 14:38:07 +0200

    Worked

--- policy.py
+++ policy.py
@@ -8,10 +8,12 @@ from numpy.linalg import inv
 alpha = 2
 M = dict()
 b = dict()
-z = dict()
+w = dict()
+#z = dict()
+z_t = 0
 Artikel = {}
 t = ""
-x_t = ""
+x_t = "Nah"
 
 # Evaluator will call this function and pass the article features.
 # Check evaluator.py description for details.
@@ -27,27 +29,30 @@ def set_articles(art):
 # Check task description for details.
 def update(reward):
     global M, b
+    if reward == -1:
+        return
     y_t = reward
     #for art in Artikel:
     x = str(x_t)
-    M[x] = np.add(M[x], np.dot(z[t], z[t].transpose()))
-    b[x] = np.add(b[x], np.multiply(z[t], y_t))
+    M[x] = np.add(M[x], np.dot(z_t, z_t.transpose()))
+    b[x] = np.add(b[x], np.multiply(z_t, y_t))
 
 # This function will be called by the evaluator.
 # Check task description for details.
 def reccomend(timestamp, user_features, articles):
-    global t, z, M, b, x_t
+    global t, z_t, M, b, x_t, w
     t = str(timestamp)
-    z[t] = np.array(user_features)
+    z_t = np.array(user_features)
     Zaehler = 0
     Ausweis = 0
     for art in articles:
         Zaehler += 1
         x = str(art)
 
-        w_x = np.dot(inv(M[x]), b[x])
+        if x_t == "Nah" or x == x_t:
+            w[x] = np.dot(inv(M[x]), b[x])
 
-        UCB = np.dot( w_x.transpose(), z[t] ) + np.multiply(np.sqrt(np.dot(np.dot(z[t].transpose(), inv(M[x])), z[t])), alpha)
+        UCB = np.dot( w[x].transpose(), z_t ) + np.multiply(np.sqrt(np.dot(np.dot(z_t.transpose(), inv(M[x])), z_t)), alpha)
         if Zaehler == 1 or UCB > antwort:
             antwort = UCB
             Ausweis = art
